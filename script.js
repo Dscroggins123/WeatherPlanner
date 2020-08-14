8511468fc534833a5defd64b48124ae932c905aa
Dustin Scroggins <dscroggins123@gmail.com>
2020-08-14 15:40:10 -0700

    added local storage functionality and 5 day function

--- script.js
+++ script.js
@@ -1,7 +1,17 @@
 
 
 var searchButton = $("#searchButton")
-
+var Saved = () => {
+    $("#cities").html("")
+cities.forEach(element => {
+ $("#cities").append(`<button class="btn btn-primary mb-1">${element}</button>`)
+});
+}
+var cities = [];
+if(localStorage.getItem("cities")){
+   cities = JSON.parse(localStorage.getItem('cities'))
+}
+Saved()
 
 $(searchButton).on("click", function(event){
     event.preventDefault();
@@ -10,9 +20,6 @@ $(searchButton).on("click", function(event){
 
     
     
-     
-    localStorage.setItem("cities", JSON.stringify(inputCity) );
-    var cities = JSON.parse(localStorage.getItem('cities'))
     
     
     var ourQueryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + inputCity + "&APPID=98c6bb55a308a516a74756820c25e107";
@@ -21,9 +28,15 @@ $(searchButton).on("click", function(event){
         url: ourQueryURL,
         method: "GET"
     }).then(function(response){
+        if(!cities.includes(inputCity)){
+           cities.push(inputCity) 
+        }
+        localStorage.setItem("cities", JSON.stringify(cities) );
+        Saved()
         console.log(response.weather[0].icon )
         var temperature = (response.main.temp - 273.15) * (1.8) + 32 
-        $("#cityName").html(response.name )
+        $("#cityName").html(response.name + "<img src=http://openweathermap.org/img/wn/" + response.weather[0].icon + ".png" + ">")
+        $("#cityName")
         $("#temperature").html("Temperature: " + temperature + " &#8457;") 
         $("#humidity").html("Humidity: " + response.main.humidity + " %")
         $("#windSpeed").html("Wind Speed: " + response.wind.speed + " MPH")
@@ -35,24 +48,27 @@ $(searchButton).on("click", function(event){
     function getUV (latitude,longitude){
     //   console.log(latitude)
     //   console.log(longitude)
-     var ourQueryURL =  "https://api.openweathermap.org/data/2.5/onecall?lat=" + latitude + "&lon=" + longitude + "&exclude=hourly,daily&appid=98c6bb55a308a516a74756820c25e107"
+     var ourQueryURL =  "https://api.openweathermap.org/data/2.5/onecall?lat=" + latitude + "&lon=" + longitude + "&exclude=hourly&appid=98c6bb55a308a516a74756820c25e107"
      
       
      $.ajax({
         url: ourQueryURL,
         method: "GET"
     }).then(function(secondResponse){
-        console.log(secondResponse)
+        secondResponse.daily.forEach(fiveday => {
+            
+        });
+        // console.log(secondResponse)
         $("#UV").html("UV Index: " + secondResponse.current.uvi)
         
     })
 
 }
 
-function fiveDayForecast (){
 
-}
-$("#cities").append("<br>" + cities)
+
+
+
 
 
     

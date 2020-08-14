a802738e0c71b7d1a2a693139480896b8297eef2
Dustin Scroggins <dscroggins123@gmail.com>
2020-08-14 16:06:48 -0700

    appended text to my 5 day forecast card

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-
-
+var date = new Date();
+var currentDate = `${date.getMonth()+1}/${date.getDate()}/${date.getFullYear()}`
 var searchButton = $("#searchButton")
 var Saved = () => {
     $("#cities").html("")
@@ -35,7 +35,7 @@ $(searchButton).on("click", function(event){
         Saved()
         console.log(response.weather[0].icon )
         var temperature = (response.main.temp - 273.15) * (1.8) + 32 
-        $("#cityName").html(response.name + "<img src=http://openweathermap.org/img/wn/" + response.weather[0].icon + ".png" + ">")
+        $("#cityName").html(response.name + currentDate + "<img src=http://openweathermap.org/img/wn/" + response.weather[0].icon + ".png" + ">")
         $("#cityName")
         $("#temperature").html("Temperature: " + temperature + " &#8457;") 
         $("#humidity").html("Humidity: " + response.main.humidity + " %")
@@ -48,17 +48,26 @@ $(searchButton).on("click", function(event){
     function getUV (latitude,longitude){
     //   console.log(latitude)
     //   console.log(longitude)
-     var ourQueryURL =  "https://api.openweathermap.org/data/2.5/onecall?lat=" + latitude + "&lon=" + longitude + "&exclude=hourly&appid=98c6bb55a308a516a74756820c25e107"
+     var ourQueryURL =  "https://api.openweathermap.org/data/2.5/onecall?lat=" + latitude + "&lon=" + longitude + "&exclude=hourly&units=imperial&appid=98c6bb55a308a516a74756820c25e107"
      
-      
+    
      $.ajax({
         url: ourQueryURL,
         method: "GET"
     }).then(function(secondResponse){
-        secondResponse.daily.forEach(fiveday => {
-            
+        secondResponse.daily.forEach((day, i) => {
+            if(i < 5){
+                $("#fiveday").append(`<div class="card col-2">
+                <div class="card-body">
+                  <h5 class="card-title date" >${currentDate.replace(/\/(.+)\//, a=> `/${+a.replace(/\//g, "")+i+1}/`)}</h5>
+                  <h6 class="card-subtitle mb-2 text-muted temperature" >${day.temp.day + " &#8457;"}</h6>
+                  <p class="card-text humidity">${day.humidity + " %"}</p>
+                  
+                </div>
+              </div>`)
+            }
         });
-        // console.log(secondResponse)
+        console.log(secondResponse)
         $("#UV").html("UV Index: " + secondResponse.current.uvi)
         
     })

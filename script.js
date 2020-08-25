2db9a4022560f4b09f80c0b908c107e4b7b92a6d
Dustin Scroggins <dscroggins123@gmail.com>
2020-08-25 15:37:09 -0700

    finished click functionality

--- script.js
+++ script.js
@@ -4,17 +4,26 @@ var searchButton = $("#searchButton")
 var Saved = () => {
     $("#cities").html("")
 cities.forEach(element => {
- $("#cities").append(`<button class="btn btn-primary mb-1">${element}</button>`)
+ $("#cities").append(`<button class="btn btn-primary mb-1 added-cities" value=${element}>${element}</button>`)
 });
+
 }
 var cities = [];
 if(localStorage.getItem("cities")){
    cities = JSON.parse(localStorage.getItem('cities'))
 }
 Saved()
+$(document).on("click", ".added-cities", (e) => {
+    e.preventDefault()
+    const inputCity = e.target.value
+    var ourQueryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + inputCity + "&APPID=98c6bb55a308a516a74756820c25e107";
+    searchAndRender(ourQueryURL, inputCity);
+})
 
 $(searchButton).on("click", function(event){
-    event.preventDefault();
+    $("<div>").hide("fast", function City(){
+        event.preventDefault();
+    
     var inputCity = $(".inputField").val()
     console.log(inputCity)
 
@@ -22,20 +31,29 @@ $(searchButton).on("click", function(event){
     
     
     
-    var ourQueryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + inputCity + "&APPID=98c6bb55a308a516a74756820c25e107";
+    var ourQueryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + inputCity + "&APPID=98c6bb55a308a516a74756820c25e107";
+    console.log(ourQueryURL)
+        searchAndRender(ourQueryURL, inputCity)
     
+})
+
+
+})
+
+function searchAndRender(url, city) {
+        
     $.ajax({
-        url: ourQueryURL,
+        url: url,
         method: "GET"
     }).then(function(response){
-        if(!cities.includes(inputCity)){
-           cities.push(inputCity) 
+        if(!cities.includes(city)){
+           cities.push(city) 
         }
         localStorage.setItem("cities", JSON.stringify(cities) );
         Saved()
-        console.log(response.weather[0].icon )
-        var temperature = (response.main.temp - 273.15) * (1.8) + 32 
-        $("#cityName").html(response.name + " " + currentDate + "<img src=http://openweathermap.org/img/wn/" + response.weather[0].icon + ".png" + ">")
+        console.log(response)
+        var temperature = Math.round((response.main.temp - 273.15) * (1.8) + 32 )
+        $("#cityName").html(response.name + " (" + currentDate + ")" + "<img src=https://openweathermap.org/img/wn/" + response.weather[0].icon + ".png" + ">")
         $("#cityName")
         $("#temperature").html("Temperature: " + temperature + " &#8457;") 
         $("#humidity").html("Humidity: " + response.main.humidity + " %")
@@ -44,10 +62,12 @@ $(searchButton).on("click", function(event){
         var longitude = response.coord.lon
         getUV (latitude,longitude)
     })
-    
-    function getUV (latitude,longitude){
+}
+
+function getUV (latitude,longitude){
     //   console.log(latitude)
     //   console.log(longitude)
+    $("#fiveday").empty();
      var ourQueryURL =  "https://api.openweathermap.org/data/2.5/onecall?lat=" + latitude + "&lon=" + longitude + "&exclude=hourly&units=imperial&appid=98c6bb55a308a516a74756820c25e107"
      
     
@@ -56,31 +76,27 @@ $(searchButton).on("click", function(event){
         method: "GET"
     }).then(function(secondResponse){
         secondResponse.daily.forEach((day, i) => {
-            if(i < 5){
-                $("#fiveday").append(`<div class="card col-2" style="width: 70px;height: 150px;" style="height: 100px;" >
-                <div>
+            if(i < 5){ $("#fiveday").append
+                $("#fiveday").append(`<div class="card col-2" style="width: 130px;" style="height: 100px;" >
+                <div style="height: 130px;">
                   <h5 class="card-title date" style="font-size:15px">${currentDate.replace(/\/(.+)\//, a=> `/${+a.replace(/\//g, "")+i+1}/`)}</h5>
                   
-                  <p class="card-text temperature" style="font-size:12px" >${"Temp: " + day.temp.day + "&#8457;"}</h6>
+                  <p class="card-text temperature" style="font-size:12px" >${"Temp: " + Math.round(day.temp.day) + "&#8457;"}</h6>
                   <p class="card-text humidity" style="font-size:12px"> ${"Humidity: " + day.humidity + "%"}</p>
                   
                 </div>
               </div>`)
             }
         });
+        
         console.log(secondResponse)
-        $("#UV").html("UV Index: " + secondResponse.current.uvi)
+        $("#UV").text("UV Index: " + secondResponse.current.uvi)
         
     })
+    
 }
 
 
-
-
-
-
-
-
     
     
 
@@ -100,5 +116,3 @@ $(searchButton).on("click", function(event){
 
 
 
-
-})
